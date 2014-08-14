ce1af4f8b0c3e46a477c365bc698bab809baf632
Klaas Pieter Annema <klaaspieter@annema.me>
2014-08-14 10:21:31 -0400

    Don't trigger tableview push segue twice

--- objc-io-issue-15-ux-testing/ViewController.m
+++ objc-io-issue-15-ux-testing/ViewController.m
@@ -20,6 +20,13 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)viewWillAppear:(BOOL)animated;
+{
+    [super viewWillAppear:animated];
+
+    [self.tableView deselectRowAtIndexPath:self.tableView.indexPathForSelectedRow animated:animated];
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
